3a7de67b30bdbf58a288e75c70ccd17fa1e8dc18
RitaSoaresss <151063327+RitaSoaresss@users.noreply.github.com>
2024-01-02 20:06:57 +0000

    Delete FASTQC_SH.sh

--- FASTQC_SH.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-for n in $(ls /mnt/nfs/lobo/SALMEIDA-NFS/lcosta/MCPP-Lung/NEW_ILLUMINA_PIPELINE/untrimmed_fastq/*_R2_001.fastq.gz)
-do
- echo $n
- sbatch --export=sample=$n,ALL FASTQC_new_illumina.sbatch
-done
